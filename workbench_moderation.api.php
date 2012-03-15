b04fbf7ccfc4b5632e8f537909ae69e0c6672e76
muriqui <muriqui@110967.no-reply.drupal.org>
2012-03-15 11:40:43 -0500

    Issue #1108502: by muriqui, ericduran, Adding hook_workbench_moderation_states_next_alter()

--- workbench_moderation.api.php
+++ workbench_moderation.api.php
@@ -28,6 +28,30 @@ function hook_workbench_moderation_access_alter(&$access, $op, $node) {
 }
 
 /**
+ * Allows modules to alter the list of possible next states for a node.
+ *
+ * @param &$states
+ *   An array of possible state changes, or FALSE if none were found before
+ *   invoking this hook. Passed by reference.
+ * @param $current_state
+ *   The current moderation state.
+ * @param $context
+ *   An associative array containing:
+ *   - 'account': The user object being checked.
+ *   - 'node': The node object being acted upon.
+ *
+ * @see workbench_moderation_states_next()
+ */
+function hook_workbench_moderation_states_next_alter(&$states, $current_state, $context) {
+  // Do not permit users to give final approval to their own nodes, even if
+  // they would otherwise have rights to do so.
+  $published = workbench_moderation_state_published();
+  if (isset($states[$published]) && ($context['account']->uid == $context['node']->uid)) {
+    unset($states[$published]);
+  }
+}
+
+/**
  * Allows modules to respond to state transitions.
  *
  * @param $node
